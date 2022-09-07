32a15490a7ea681a4a921672f4a12340746b630d
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-09-07 14:17:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php require_once("header.php"); ?>
 <h1>My Website</h1>
+
+<form action="handlepost.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
 <?php require_once("footer.php"); ?>
