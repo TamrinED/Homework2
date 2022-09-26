bcb15f9d6b773ce03eae5c6df943c7a3a39f06a4
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-09-26 14:45:34 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -1,14 +1,13 @@
 <?php require_once("header.php"); ?>
-<?php require_once("ThankYouMessage.php"); ?>
+
 
   <h1>Post Response</h1>
   Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
 <body>
-<?php ThankYouMessage(); ?>
+
   
   <a class"btn btn-primary" href="page2.html" role="button">Page 2</a>
-</body>
+<?php require_once("footer.php"); ?>
   
 
-<?php require_once("footer.php"); ?>
