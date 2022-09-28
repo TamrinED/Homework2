7e4c3a08c273b09d322cf8a8acf5856b4d8e4ff2
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-09-28 14:14:15 -0500

    Update and rename index.html to get.php
    added get.php

---
+++ get.php
@@ -0,0 +1,9 @@
+<?php require_once("header.php"); ?>
+    
+<form action="handleget.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+    
+<?php require_once("footer.php"); ?>
