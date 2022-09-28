7a34f65a33c7fcd8fba29dcc778c903d986d0e8e
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-09-28 14:03:38 -0500

    Update ThankYouMessage.php

--- ThankYouMessage.php
+++ ThankYouMessage.php
@@ -1,9 +1,4 @@
-<?php
-require_once("header.php"); ?>
-
-<?php function ThankYouMessage{
-  echo Thank you for testing my website
+<?php function ThankYouMessage() {
+  echo "Thank you for testing my website";
 };
 
-<?php
-require_once("footer.php"); ?>
