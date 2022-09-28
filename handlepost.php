bce8dbee5f99ea41ebadc4774d4c8448534f70fe
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-09-28 13:45:20 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -4,7 +4,7 @@
   <h1>Post Response</h1>
   Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
-<body>
+
 
   
   <a class"btn btn-primary" href="page2.html" role="button">Page 2</a>
